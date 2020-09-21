eb22d0a0f237059493d69b33c24bc625c1846664
TurtleSun <57500228+TurtleSun@users.noreply.github.com>
2020-09-20 21:41:27 -0400

    .

---
+++ app.js
@@ -0,0 +1,20 @@
+var life = 100;
+
+life = life - 10;
+
+var name = "Nuggs";
+
+console.log(life);
+console.log(name);
+
+
+
+//const cannot modify values in var;
+//ie const life = 100; and const life = 1000; equal no workie
+
+//HOWEVER if you use let instead of const it allows the 
+//change varible 
+
+//fun facts: reserved terms such as false need no quotes
+//varibles with no equal sign are undefined n have no value
+//"VAR" has no value? 
